758b21bdd0f3617a4bd3219f68ab6055e499cbe0
Andrei Gatea <andreigatea@gmail.com>
2016-08-25 19:28:59 +0300

    .....

--- web/client/source/js/modules/person/config/routes.js
+++ web/client/source/js/modules/person/config/routes.js
@@ -17,6 +17,7 @@
             .state( 'layout.personform', {
                 url: '^/personform',
                 parent: 'layout',
+                controller: 'personCtrl',
                 templateUrl: 'views/modules/person/addNew.html'
             } )
             .state( 'layout.test', {
